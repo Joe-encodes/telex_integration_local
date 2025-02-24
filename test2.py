fd49da2a9385c064250c0788e0c152f767ef6166
Joe-encodes <josephadqmu2019@gmail.com>
2025-02-24 14:45:21 +0100

    `Removed all unecessary code files from the repository.`

--- test2.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from main import generate_outreach_email
-
-async def test_aimlapi():
-    content = await generate_outreach_email("Test website", "https://.com", "SEO improvement services")
-    print("AIMLAPI Response:", content)
-
-if __name__ == "__main__":
-    asyncio.run(test_aimlapi())
